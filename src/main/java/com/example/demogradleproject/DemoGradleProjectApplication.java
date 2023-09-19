8506e7624fcd0c15af8fb169d143da8f6eac133f
faiyaz666 <faiyazrafid5@gmail.com>
2023-09-19 20:47:36 +0600

    spring boot project created

---
+++ src/main/java/com/example/demogradleproject/DemoGradleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogradleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGradleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGradleProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demogradleproject/model/Computer.java
@@ -0,0 +1,4 @@
+package com.example.demogradleproject.model;
+
+public class Computer {
+}
